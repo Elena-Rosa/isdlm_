d5538c7933cb182f2149535d5ce47711eb9522f8
Elena <elenarosamail@gmail.com>
2024-02-29 15:56:22 -0800

    typewriter effect with testing

---
+++ .vscode/README.md
@@ -0,0 +1,3 @@
+researching and planning phase 
+
+completing various short games to learn the basics of unity. Have just completed a bird game. Similar to an iphone early bird game
---
+++ Assets/_testing/Scripts/Testing_Architect.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TESTING
+{
+    public class Testing_Architect : MonoBehaviour
+
+    {
+        DialogueSystem ds;
+        TextArchitect architect;
+
+        string[] lines = new string[6]
+        {
+            "Hi, where do I begin.",
+            "And where do I end!",
+            "With her, or with me.",
+            "Somewhere...",
+            "It's nice to meet you in here!",
+            "I'll see you around..."
+        };
+
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            ds = DialogueSystem.instance;
+            architect = new TextArchitect(ds.dialogueContainer.dialogueText);
+            architect.buildMethod = TextArchitect.BuildMethod.typewriter;
+            architect.speed = 0.5f;
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            string longLine = "wow this is the longest line ever in history, wow, it just keeps going and going, I am not sure where I end or begin. When You loose someone, where does the loss go, can you ever find them again? I am curious about that. ";
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                if (architect.isBuilding)
+                {
+                    if (!architect.hurryUp)
+                        architect.hurryUp = true;
+                    else
+                        architect.ForceComplete();
+                }
+                else
+                    architect.Build(longLine);
+                //architect.Build(lines[Random.Range(0, lines.Length)]);
+            }
+            else if (Input.GetKeyDown(KeyCode.A))
+            {
+                architect.Append(longLine);
+                //architect.Append(lines[Random.Range(0, lines.Length)]);
+            }
+        }
+    }
+}
+
